b9930f91a179eb77b67099107a15efeaaf668bd0
jeremyg <jeremygirard89100@gmail.com>
2026-01-21 16:08:21 +0100

    add TP9 v1

---
+++ TP9/GameView.js
@@ -0,0 +1,76 @@
+// python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+
+class GameView {
+  constructor(game) {
+    this.game = game;
+    this.canvas = document.querySelector("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+
+    // Chargement du fond
+    this.background = new Image();
+    this.background.src = "img/jsarena.jpg";
+    this.background.onload = () => this.drawBackground();
+
+    this.playerSkin = {};
+
+    this.render();
+  }
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  drawBackground() {
+    this.ctx.drawImage(this.background, 0, 0, this.width, this.height);
+  }
+
+  render() {
+    this.clear();
+    this.drawBackground();
+    const players = this.game.infos.players;
+    for (const id in players) {
+      const player = players[id];
+
+      if (player != player.isDead) {
+        player.animate();
+        this.drawPlayer(player);
+      }
+    }
+  }
+
+  drawPlayer(player) {
+    let skinPath = player.skinPath;
+
+    // Débug du skinPath
+    if (!skinPath.startsWith("./") && !skinPath.startsWith("http")) {
+      skinPath = "./" + skinPath;
+    }
+
+    // Initialisation du skin du player
+    if (!this.playerSkin[skinPath]) {
+      this.playerSkin[skinPath] = new Image();
+      this.playerSkin[skinPath].src = player.skinPath;
+    }
+    const skin = this.playerSkin[skinPath];
+
+    const x = player.renderX * this.width;
+    const y = player.renderY * this.height;
+
+    let spriteX = 0;
+    let spriteY = 0;
+    const spriteWidth = 64;
+    const spriteHeight = 64;
+
+    this.ctx.drawImage(
+      skin,
+      spriteX,
+      spriteY,
+      spriteWidth,
+      spriteHeight,
+      x - spriteWidth / 2,
+      y - spriteHeight / 2,
+      spriteWidth,
+      spriteHeight,
+    );
+  }
+}
---
+++ TP9/portail.js
@@ -0,0 +1,79 @@
+const liste = document.querySelector("#choixPerso");
+
+// Initialisation de skinPath
+let skinPath = null;
+
+// Boucle qui parcoure toutes les images présentes dans assets
+for (let i = 1; i <= 29; i++) {
+  const img = new Image();
+  img.src = `./assets/${i}.png`;
+
+  const canvas = document.createElement("canvas"); // Création canvas
+
+  img.onload = () => {
+    canvas.width = 64;
+    canvas.height = 64;
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(
+      img,
+      0,
+      128,
+      canvas.width,
+      canvas.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height,
+    );
+    // ctx.drawImage(img, coordonée x du coin supérieur gauche, coordonnée y du coin supérieur gauche,
+    // largeur de la zone de l'image, hauteur de la zone de l'image, coordonnée x où coller l'image,
+    //  coordonnée y où coller l'image, largeur de l'image, hauteur de l'image)
+
+    liste.appendChild(canvas);
+
+    // Ecouteur placé pour récupérer le skin au clic
+    canvas.addEventListener("click", function () {
+      skinPath = `./assets/${i}.png`;
+
+      // Initialisation de la variable qui va récupérer tous les skins
+      const getSkins = liste.querySelectorAll("canvas");
+
+      // Boucle tous les skins et retire le css .sélection lors d'un clic
+      getSkins.forEach(function (skins) {
+        skins.classList.remove("selected");
+      });
+
+      // Ajoute le css .selected lors d'un clic
+      canvas.classList.add("selected");
+    });
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#skins");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // empêche le rechargement de la page
+
+    // Récupère les valeurs, trim permet de retirer les espaces
+    const pseudo = document.querySelector("#name").value.trim();
+    const serverUrl = document.querySelector("#url").value.trim();
+
+    // Message d'alerte si une ou plusieurs données ne sont pas présentes
+    if (pseudo === "" || serverUrl === "" || skinPath === null) {
+      const joinButton = document.querySelector("#joinButton");
+      const alert = document.createElement("p");
+      alert.textContent = "Erreur : information(s) manquante(s)";
+      form.insertBefore(alert, joinButton);
+      return;
+    }
+
+    let monStockage = localStorage;
+    monStockage.setItem("name", pseudo); // Stock pseudo dans le storage
+    monStockage.setItem("url", serverUrl);
+    monStockage.setItem("click", skinPath);
+
+    console.log(pseudo, serverUrl, skinPath);
+
+    window.location = "game.html";
+  });
+});
